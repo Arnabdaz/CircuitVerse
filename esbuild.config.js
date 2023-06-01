033360a4453ac05872bc3ac157ae37c3467dbe71
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-06-01 13:20:14 +0530

    feat(esbuild): update esbuild, and change submodule for testing
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 const esbuild = require('esbuild');
 const rails = require('esbuild-rails');
 const sassPlugin = require('esbuild-plugin-sass');
+const { execSync } = require('child_process');
 
 const watchDirectories = [
     './app/javascript/**/*.js',
     './app/views/**/*.html.erb',
     './app/assets/stylesheets/**/*',
+    './cv-frontend-vue/**/*',
 ];
 
 const watch = process.argv.includes('--watch');
@@ -15,24 +17,37 @@ const watchPlugin = {
     name: 'watchPlugin',
     setup(build) {
         build.onStart(() => {
-            // eslint-disable-next-line no-console
             console.log(`Build starting: ${new Date(Date.now()).toLocaleString()}`);
         });
         build.onEnd((result) => {
             if (result.errors.length > 0) {
-                // eslint-disable-next-line no-console
                 console.error(`Build finished, with errors: ${new Date(Date.now()).toLocaleString()}`);
             } else {
-                // eslint-disable-next-line no-console
                 console.log(`Build finished successfully: ${new Date(Date.now()).toLocaleString()}`);
             }
         });
     },
 };
 
+async function buildVue() {
+    execSync('git submodule update --init --remote', { cwd: process.cwd() });
+    execSync('npm install', { cwd: path.join(process.cwd(), 'cv-frontend-vue') });
+    execSync('npm run build', { cwd: path.join(process.cwd(), 'cv-frontend-vue') });
+}
+
+const vuePlugin = {
+    name: 'vuePlugin',
+    setup(build) {
+        build.onStart(() => {
+            console.log(`Building Vue site: ${new Date(Date.now()).toLocaleString()}`);
+            buildVue();
+        });
+    },
+};
+
 async function run() {
     const context = await esbuild.context({
-        entryPoints: ['application.js', 'simulator.js', 'testbench.js'],
+        entryPoints: ['application.js', 'simulator.js', 'testbench.js', './cv-frontend-vue/src/main.js'],
         bundle: true,
         outdir: path.join(process.cwd(), 'app/assets/builds'),
         absWorkingDir: path.join(process.cwd(), 'app/javascript'),
@@ -40,7 +55,7 @@ async function run() {
         loader: {
             '.png': 'file', '.svg': 'file', '.ttf': 'file', '.woff': 'file', '.woff2': 'file', '.eot': 'file',
         },
-        plugins: [rails(), sassPlugin(), watchPlugin],
+        plugins: [rails(), sassPlugin(), vuePlugin, watchPlugin],
     });
 
     if (watch) {

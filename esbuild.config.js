b0333dcca633ab6be2014e1e71e21ced44c391c8
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-06-01 16:34:17 +0530

    feat(final): final working version of esbuild
    Signed-off-by: Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>

--- esbuild.config.js
+++ esbuild.config.js
@@ -3,13 +3,12 @@ const esbuild = require('esbuild');
 const rails = require('esbuild-rails');
 const sassPlugin = require('esbuild-plugin-sass');
 const { execSync } = require('child_process');
-const chokidar = require('chokidar');
+const readline = require('readline');
 
 const watchDirectories = [
     './app/javascript/**/*.js',
     './app/views/**/*.html.erb',
     './app/assets/stylesheets/**/*',
-    './cv-frontend-vue/**/*',
 ];
 
 const watch = process.argv.includes('--watch');
@@ -17,14 +16,11 @@ const watch = process.argv.includes('--watch');
 const watchPlugin = {
     name: 'watchPlugin',
     setup(build) {
-        // eslint-disable-next-line no-console
         console.log(`Build starting: ${new Date(Date.now()).toLocaleString()}`);
         build.onEnd((result) => {
             if (result.errors.length > 0) {
-                // eslint-disable-next-line no-console
                 console.error(`Build finished, with errors: ${new Date(Date.now()).toLocaleString()}`);
             } else {
-                // eslint-disable-next-line no-console
                 console.log(`Build finished successfully: ${new Date(Date.now()).toLocaleString()}`);
             }
         });
@@ -42,6 +38,7 @@ const vuePlugin = {
     setup(build) {
         build.onStart(() => {
             console.log(`Building Vue site: ${new Date(Date.now()).toLocaleString()}`);
+            buildVue();
         });
     },
 };
@@ -60,12 +57,17 @@ async function run() {
     });
 
     if (watch) {
-        const watcher = chokidar.watch('./cv-frontend-vue', { ignoreInitial: true });
-        watcher.on('all', (event) => {
-            if (event === 'change') {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        rl.on('line', (input) => {
+            if (input.trim() === 'r' || input.trim() === 'R') {
                 execSync('npm run build', { cwd: path.join(process.cwd(), 'cv-frontend-vue') });
             }
         });
+
         await context.watch();
     } else {
         await context.rebuild();
